5bab3cee67b557c3de3d4f64d6d9a0c1e243734e
Cameron <rubiksexplosion@gmail.com>
2024-02-08 23:49:59 -0600

    added method for generating a create table command from first row column names in tab separated file
    added .gitignore with common python/jetbrains values
    
    added /data to gitignore due to github file size limits

--- preprocessing/converter.py
+++ preprocessing/converter.py
@@ -1,3 +1,23 @@
+import os
+
 
 def convertNASSCropDataToCsv(file_name_in: str, file_name_out: str):
     pass
+
+
+def genCreateTableCommand(file_path_in: str) -> str:
+    command = "CREATE TABLE crop_data ("
+    with open(file_path_in) as fin:
+        first_line = fin.readline().strip('\n')
+    first_line = first_line.split('\t')
+    for line in first_line:
+        command += '"' + line + '"' + " varchar, "
+    command = command[:len(command) - 2]  # remove extra ", "
+    command += ");"
+    return command
+
+
+if __name__ == "__main__":
+    file_name = "qs_crops_20240203.txt"
+    file_path_in = os.getcwd().replace("preprocessing", "data") + "\\" + file_name
+    print(genCreateTableCommand(file_path_in))
