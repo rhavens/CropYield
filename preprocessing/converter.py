d4e60abeb86bcae63285ff68c84074133f33cf2f
RubiksImplosion <rubiksexplosion@gmail.com>
2024-02-10 11:52:36 -0600

    added instructions for running preprocessor

--- preprocessing/converter.py
+++ preprocessing/converter.py
@@ -15,7 +15,7 @@ def preprocess_crop_file(file_path, file_name):
                 return
 
             chunk = chunk.replace(b'\00', b'')  # the null characters in the input cause issues in postgres
-            with open(file_path + file_name + "_nullremoved", 'ab') as fout:
+            with open(file_path + file_name + "_preprocessed", 'ab') as fout:
                 fout.write(chunk)
 
 
@@ -23,7 +23,6 @@ def gen_create_table_command(file_path: str, file_name: str) -> str:
     command = "CREATE TABLE crop_data ("
     with open(file_path + file_name) as fin:
         first_line = fin.readline().strip('\n')
-        second_line = fin.readline().strip('\n')
     for line in first_line:
         command += '"' + line + '"' + " varchar, "
     command = command[:len(command) - 2]  # remove extra ", "
@@ -31,6 +30,12 @@ def gen_create_table_command(file_path: str, file_name: str) -> str:
     return command
 
 
+"""
+TO USE:
+Place the qscrop file in the /data folder
+Set "file_name" to the name of the qs crop file
+The output will be a new file named "<filename>_preprocessed" in the /data folder
+"""
 if __name__ == "__main__":
     file_name = "qs.crops_20240203.txt"
     file_path = os.getcwd().replace("preprocessing", "data") + "\\"
