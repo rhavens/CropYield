d47b06d2ac87592ab97622de1c6ca0144e4fe537
rhavens <114414576+rhavens@users.noreply.github.com>
2024-02-14 13:23:36 -0600

    Add files via upload

---
+++ model.py
@@ -0,0 +1,16 @@
+import numpy as np
+import matplotlib as plt
+import pandas as pd
+
+# Main program file for running predictive models
+# Program is structure with functions on top, driver code on bottom
+
+
+
+
+
+
+
+
+
+
