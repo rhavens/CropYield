3a0f01d138fd9eee09a3c27fdb2b9ce77e393315
Ryan Havens <rhavens139@gmail.com>
2024-04-05 21:24:10 -0500

    Updated js file

--- frontend/crop-yield/src/App.js
+++ frontend/crop-yield/src/App.js
@@ -3,113 +3,13 @@ import './App.css';
 import cornImage from './cornimage.jpeg';
 import Chart from 'chart.js/auto'; // Import Chart.js
 
-/*
-    Format of object return
-    map = {
-        state_name: {
-            code: value
-            counties: {
-                county_name: value
-                county_name: value
-                ...
-            }
-        }
-        ...
-    }
-     */
-// async function createStateCountyCodeMap() {
-//     const response = await fetch('./name_code_map.csv');
-//     const text = await response.text();
-//     // const text = fs.readFileSync('./name_code_map.csv')
-//     const rows = text.split('\n');
-//     const data = rows.map(row => row.split(','));
-//
-//     const map = {}
-//
-//     data.forEach(row => {
-//         const state_name = row[0]
-//         const state_code = Number(row[1])
-//         const county_name = row[2]
-//         const county_code = Number(row[3])
-//
-//         if (state_name in map) {
-//             map[state_name]["counties"][county_name] = county_code;
-//         } else {
-//             map[state_name] = {};
-//             map[state_name][state_code] = state_code;
-//             map[state_name]["counties"] = {}
-//             map[state_name]["counties"][county_name] = county_code;
-//         }
-//     });
-//     setStateCountyCodeMap(map);
-// }
-
 function App() {
-    const [countyCode, setCountyCode] = useState('');
-    const [stateCode, setStateCode] = useState('');
-    const [predictionResult, setPredictionResult] = useState(null);
-    const [cleanedData, setCleanedData] = useState(null); // State to hold cleaned data
-    const [error, setError] = useState(null);
-    const [formSubmitted, setFormSubmitted] = useState(false);
-    const [stateCountyCodeMap, setStateCountyCodeMap] = useState(null);
-
-    if (stateCountyCodeMap == null) {
-        fetch('./name_code_map.csv')
-            .then(response => response.text())
-            .then(text => {
-                const rows = text.split('\n');
-                const data = rows.map(row => row.split(','));
-
-                const map = {};
-
-                data.forEach(row => {
-                    const state_name = row[0];
-                    const state_code = Number(row[1]);
-                    const county_name = row[2];
-                    const county_code = Number(row[3]);
-
-                    if (state_name in map) {
-                        map[state_name]["counties"][county_name] = county_code;
-                    } else {
-                        map[state_name] = {};
-                        map[state_name][state_code] = state_code;
-                        map[state_name]["counties"] = {};
-                        map[state_name]["counties"][county_name] = county_code;
-                    }
-                });
-                setStateCountyCodeMap(map);
-            })
-            .catch(error => {
-                console.error('Error fetching the CSV file:', error);
-            });
-    }
-
-    // async function createStateCountyCodeMap() {
-    //     const response = await fetch('./name_code_map.csv');
-    //     const text = await response.text();
-    //     // const text = fs.readFileSync('./name_code_map.csv')
-    //     const rows = text.split('\n');
-    //     const data = rows.map(row => row.split(','));
-    //
-    //     const map = {}
-    //
-    //     data.forEach(row => {
-    //         const state_name = row[0]
-    //         const state_code = Number(row[1])
-    //         const county_name = row[2]
-    //         const county_code = Number(row[3])
-    //
-    //         if (state_name in map) {
-    //             map[state_name]["counties"][county_name] = county_code;
-    //         } else {
-    //             map[state_name] = {};
-    //             map[state_name][state_code] = state_code;
-    //             map[state_name]["counties"] = {}
-    //             map[state_name]["counties"][county_name] = county_code;
-    //         }
-    //     });
-    //     setStateCountyCodeMap(map);
-    // }
+  const [countyCode, setCountyCode] = useState('');
+  const [stateCode, setStateCode] = useState('');
+  const [predictionResult, setPredictionResult] = useState(null);
+  const [cleanedData, setCleanedData] = useState(null); // State to hold cleaned data
+  const [error, setError] = useState(null);
+  const [formSubmitted, setFormSubmitted] = useState(false);
 
     const handleCountyCodeChange = (event) => {
         setCountyCode(event.target.value);
@@ -119,130 +19,90 @@ function App() {
         setStateCode(event.target.value);
     };
 
-    const handleSubmit = async (event) => {
-        event.preventDefault();
-        setFormSubmitted(true);
-        setPredictionResult(null); // Clear prediction result
-        setCleanedData(null);
-
-        try {
-            const response = await fetch("http://127.0.0.1:5000/predict", {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json'
-                },
-                body: JSON.stringify({
-                    COUNTY_CODE: parseInt(countyCode),
-                    STATE_CODE: parseInt(stateCode)
-                })
-            });
-
-            const data = await response.json();
-            console.log(data);
-
-            setPredictionResult(data.nextYearPrediction);
-            setCleanedData(data.cleaned_csv_data);
-        } catch (error) {
-            console.error('Error:', error);
-            setError(error.message);
-        }
-    };
-
-    const prediction = predictionResult && predictionResult.length > 0 ? predictionResult[0] : null;
-
-    // Draw the scatter plot when cleaned data and prediction result are available
-    useEffect(() => {
-        if (cleanedData && prediction !== null) {
-            const ctx = document.getElementById('scatterChart').getContext('2d');
-
-            // Extract x and y data from cleanedData
-            const xData = cleanedData.map(entry => entry.YEAR);
-            const yData = cleanedData.map(entry => entry.VALUE);
-
-            const nextYear = new Date().getFullYear() + 1;
-
-            const scatterChartData = {
-                datasets: [{
-                    label: 'Crop Yield Over Years',
-                    data: xData.map((x, i) => ({x, y: yData[i]})),
-                    backgroundColor: 'blue',
-                    pointRadius: 5,
-                    pointHoverRadius: 8,
-                },
-                    {
-                        label: 'Next Year Prediction',
-                        data: [{x: nextYear, y: prediction}],
-                        backgroundColor: 'red',
-                        pointRadius: 8,
-                        pointHoverRadius: 10,
-                    }]
-            };
-
-            new Chart(ctx, {
-                type: 'scatter',
-                data: scatterChartData,
-                options: {
-                    scales: {
-                        x: {
-                            type: 'linear',
-                            position: 'bottom',
-                            title: {
-                                display: true,
-                                text: 'Year'
-                            }
-                        },
-                        y: {
-                            title: {
-                                display: true,
-                                text: 'Yield'
-                            }
-                        }
-                    }
-                }
-            });
-        }
-    }, [cleanedData, prediction]);
-
-
-    function generateStateDropdown() {
-        console.log("dropdown");
-        const dropdown = document.getElementById("state_name");
-        console.log(stateCountyCodeMap);
-        for (let state in stateCountyCodeMap) {
-            const option = document.createElement("option");
-            option.value = stateCountyCodeMap[state]["code"];
-            option.text = state;
-            dropdown.appendChild(option)
-        }
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    setFormSubmitted(true);
+    setPredictionResult(null); // Clear prediction result
+    setCleanedData(null);
+    
+    try {
+      const response = await fetch("http://127.0.0.1:5000/predict", {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+          COUNTY_CODE: parseInt(countyCode),
+          STATE_CODE: parseInt(stateCode)
+        })
+      });
+      
+      const data = await response.json();
+      console.log(data);
+      
+      setPredictionResult(data.nextYearPrediction);
+      setCleanedData(data.cleaned_csv_data);
+    } catch (error) {
+      console.error('Error:', error);
+      setError(error.message);
     }
+  };
 
-    return (
-        <div className="App">
-            <header className="App-header">
-                <h1>Corn Crop Yield Prediction</h1>
-                <img src={cornImage} alt="corn-image" style={{width: '10%', height: 'auto'}}></img>
-                <p>Please select your county and state code.</p>
-                <p>The model will display future predictions for your county's yield.</p>
-                <form onSubmit={handleSubmit}>
-                    <label>
-                        State Code:
-                        <input type="number" value={stateCode} onChange={handleStateCodeChange}/>
-                    </label>
-                    <label>
-                        County Code:
-                        <input type="number" value={countyCode} onChange={handleCountyCodeChange}/>
-                    </label>
-                    <button type="submit">Submit</button>
-                </form>
-                {formSubmitted && cleanedData && predictionResult !== null && (
-                    <div>
-                        <h2>Prediction Results</h2>
-                        <p style={{fontSize: "30px"}}>Next year's predicted
-                            yield: {Math.round(predictionResult)} bushels</p>
-                        <canvas id="scatterChart" width="400" height="400"></canvas>
-                        {/* Chart container */}
-                    </div>
-                )}
+  const prediction = predictionResult && predictionResult.length > 0 ? predictionResult[0] : null;
+
+  // Draw the scatter plot when cleaned data and prediction result are available
+  useEffect(() => {
+    if (cleanedData && prediction !== null) {
+      const ctx = document.getElementById('scatterChart').getContext('2d');
+      
+      // Extract x and y data from cleanedData
+      const xData = cleanedData.map(entry => entry.YEAR);
+      const yData = cleanedData.map(entry => entry.VALUE);
+
+      const nextYear = new Date().getFullYear() + 1;
+
+      const scatterChartData = {
+        datasets: [{
+          label: 'Crop Yield Over Years',
+          data: xData.map((x, i) => ({ x, y: yData[i] })),
+          backgroundColor: 'blue',
+          pointRadius: 5,
+          pointHoverRadius: 8,
+        },
+        {
+          label: 'Next Year Prediction',
+          data: [{ x: nextYear, y: prediction }],
+          backgroundColor: 'red',
+          pointRadius: 8,
+          pointHoverRadius: 10,
+        }]
+      };
+
+      new Chart(ctx, {
+        type: 'scatter',
+        data: scatterChartData,
+        options: {
+          scales: {
+            x: {
+              type: 'linear',
+              position: 'bottom',
+              title: {
+                display: true,
+                text: 'Year'
+              }
+            },
+            y: {
+              title: {
+                display: true,
+                text: 'Yield'
+              }
+            }
+          }
+        }
+      });
+    }
+  }, [cleanedData, prediction]);
+
   return (
     <div className="App">
       <header className="App-header">
